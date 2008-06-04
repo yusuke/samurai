54b5c4bc71d79f7db74b861ad0f527041d6e8405
Yusuke Yamamoto <yusuke@mac.com>
2008-06-04 16:06:59 +0000

    fix for SMR-2 support for analyzing Full GC log with class unloading message

--- gui/src/main/java/samurai/gc/SunGCParser.java
+++ gui/src/main/java/samurai/gc/SunGCParser.java
@@ -19,57 +19,48 @@ public class SunGCParser implements ScattergramDataSourceParser {
     private static GUIResourceBundle resources = GUIResourceBundle.getInstance();
     private boolean labelSet = false;
     private double memoryMax = 0;
+    private boolean unloadingClasses = false;
 
     public SunGCParser() {
     }
 
     public boolean parse(String line, ScattergramRenderer renderer) {
         try {
-            if (-1 != line.indexOf("[GC ") || -1 != line.indexOf("[Full GC ")) {
+            if (-1 != line.indexOf("[GC ") || -1 != line.indexOf("[Full GC ") || unloadingClasses || -1 != line.indexOf("[Unloading class")) {
                 if (!labelSet) {
                     renderer.setLabels(new String[]{resources.getMessage("GraphPanel.time") + "(secs)",
                             resources.getMessage("GraphPanel.memoryBeforeGC"),
                             resources.getMessage("GraphPanel.memoryAfterGC")});
                     labelSet = true;
                 }
-//        int memoryBeforeGCStart;
-//        int memoryBeforeGCEnd;
-//        int memoryAfterGCStart;
-//        int memoryAfterGCEnd;
-//        int heapStart;
-//        int heapEnd;
-//        int timeStart;
-//        int timeEnd;
 
-//    [GC 1929K->1462K(1984K), 0.0036946 secs]
-//    [Full GC 1462K->1353K(1984K), 0.1106755 secs]
-//          memoryBeforeGCStart = line.indexOf("GC ") + 3;
-//          memoryBeforeGCEnd = line.indexOf("K->");
-//          memoryAfterGCStart = line.indexOf("->") + 2;
-//          memoryAfterGCEnd = line.indexOf("K(");
-//          heapStart = line.indexOf("K(") + 2;
-//          heapEnd = line.indexOf("K)");
-//          timeStart = ;
-//          timeEnd = ;
+                unloadingClasses = -1 != line.indexOf("[Unloading class");
+                if (!unloadingClasses) {
+                    //finished unloading classes
 
-                try {
-                    double currentMemoryMax = Double.parseDouble(line.substring(line.indexOf("K(") + 2,
-                            line.indexOf("K)")));
-                    if (memoryMax < currentMemoryMax) {
-                        memoryMax = currentMemoryMax;
-                        renderer.setMaxAt(1, memoryMax);
-                        renderer.setMaxAt(2, memoryMax);
+                    try {
+                        double currentMemoryMax = Double.parseDouble(line.substring(line.indexOf("K(") + 2,
+                                line.indexOf("K)")));
+                        if (memoryMax < currentMemoryMax) {
+                            memoryMax = currentMemoryMax;
+                            renderer.setMaxAt(1, memoryMax);
+                            renderer.setMaxAt(2, memoryMax);
+                        }
+                        double time = Double.parseDouble(line.substring(line.indexOf(", ") + 2, line.lastIndexOf(" secs")));
+                        int memoryBeforeBegin = line.indexOf("GC ");
+                        if (-1 == memoryBeforeBegin) {
+                            memoryBeforeBegin = 1;
+                        } else {
+                            memoryBeforeBegin += 3;
+                        }
+                        double memoryBefore = Double.parseDouble(line.substring(memoryBeforeBegin,
+                                line.indexOf("K->")));
+                        double memoryAfter = Double.parseDouble(line.substring(line.indexOf("->") + 2,
+                                line.indexOf("K(")));
+                        renderer.addValues(new double[]{time, memoryBefore, memoryAfter});
+                        return true;
+                    } catch (NumberFormatException ignore) {
                     }
-                    double time = Double.parseDouble(line.substring(line.indexOf(", ") + 2, line.lastIndexOf(" secs")));
-                    double memoryBefore = Double.parseDouble(line.substring(line.indexOf("GC ") + 3,
-                            line.indexOf("K->")));
-                    double memoryAfter = Double.parseDouble(line.substring(line.indexOf("->") + 2,
-                            line.indexOf("K(")));
-                    renderer.addValues(new double[]{time, memoryBefore, memoryAfter});
-                    return true;
-                } catch (NumberFormatException wasNotGC) {
-//         wasNotGC.printStackTrace();
-                    //does nothing
                 }
             }
         } catch (StringIndexOutOfBoundsException sioobe) {
--- gui/src/test/java/samurai/gc/TestSunGCParser.java
+++ gui/src/test/java/samurai/gc/TestSunGCParser.java
@@ -4,7 +4,7 @@ import junit.framework.TestCase;
 
 import java.awt.Color;
 
-public class TestSunGCParser extends TestCase implements ScattergramRenderer{
+public class TestSunGCParser extends TestCase implements ScattergramRenderer {
 
     protected void setUp() throws Exception {
         super.setUp();
@@ -22,32 +22,35 @@ public class TestSunGCParser extends TestCase implements ScattergramRenderer{
 
     public void testStandardVerbosegc() throws Exception {
         SunGCParser parser = new SunGCParser();
-        expected = new double[]{0.0254331d,115008d,103309d};
-        parser.parse("[GC 115008K->103309K(129664K), 0.0254331 secs]", this);
-        expected = new double[]{0.0328756d,114957d,105199d};
-        parser.parse("[GC 114957K->105199K(129664K), 0.0328756 secs]", this);
-//        parser.parse("[Full GC[Unloading class sun.reflect.GeneratedConstructorAccessor74]", this);
-//        parser.parse("[Unloading class jsp_servlet._common._jsp.__403]", this);
-//        parser.parse("[Unloading class jsp_servlet._themes._custom._jsp.__comments]", this);
-//        parser.parse("[Unloading class sun.reflect.GeneratedMethodAccessor224]", this);
-//        parser.parse("[Unloading class jsp_servlet._styles.__global_css]", this);
-//        parser.parse("[Unloading class sun.reflect.GeneratedConstructorAccessor78]", this);
-//        parser.parse("[Unloading class jsp_servlet._themes._custom._jsp.__trackbacks]", this);
-//        parser.parse("[Unloading class jsp_servlet._themes._custom._jsp.__blogentries]", this);
-//        parser.parse("[Unloading class sun.reflect.GeneratedConstructorAccessor69]", this);
-//        parser.parse("[Unloading class sun.reflect.GeneratedConstructorAccessor75]", this);
-//        parser.parse(" 116847K->48717K(129664K), 5.4551339 secs]", this);
-        expected = new double[]{1.0320474d,61365d,51414d};
-        parser.parse("[Full GC 61365K->51414K(129664K), 1.0320474 secs]", this);
-        expected = new double[]{0.0320474d,60365d,50414d};
-        parser.parse("[GC 60365K->50414K(129664K), 0.0320474 secs]", this);
+        expected = new double[]{0.0254331d, 115008d, 103309d};
+        assertTrue(parser.parse("[GC 115008K->103309K(129664K), 0.0254331 secs]", this));
+        expected = new double[]{0.0328756d, 114957d, 105199d};
+        assertTrue(parser.parse("[GC 114957K->105199K(129664K), 0.0328756 secs]", this));
+
+        assertFalse(parser.parse("[Full GC[Unloading class sun.reflect.GeneratedConstructorAccessor74]", this));
+        assertFalse(parser.parse("[Unloading class jsp_servlet._common._jsp.__403]", this));
+        assertFalse(parser.parse("[Unloading class jsp_servlet._themes._custom._jsp.__comments]", this));
+        assertFalse(parser.parse("[Unloading class sun.reflect.GeneratedMethodAccessor224]", this));
+        assertFalse(parser.parse("[Unloading class jsp_servlet._styles.__global_css]", this));
+        assertFalse(parser.parse("[Unloading class sun.reflect.GeneratedConstructorAccessor78]", this));
+        assertFalse(parser.parse("[Unloading class jsp_servlet._themes._custom._jsp.__trackbacks]", this));
+        assertFalse(parser.parse("[Unloading class jsp_servlet._themes._custom._jsp.__blogentries]", this));
+        assertFalse(parser.parse("[Unloading class sun.reflect.GeneratedConstructorAccessor69]", this));
+        assertFalse(parser.parse("[Unloading class sun.reflect.GeneratedConstructorAccessor75]", this));
+
+        expected = new double[]{5.4551339d, 116847d, 48717d};
+        assertTrue(parser.parse(" 116847K->48717K(129664K), 5.4551339 secs]", this));
+        expected = new double[]{1.0320474d, 61365d, 51414d};
+        assertTrue(parser.parse("[Full GC 61365K->51414K(129664K), 1.0320474 secs]", this));
+        expected = new double[]{0.0320474d, 60365d, 50414d};
+        assertTrue(parser.parse("[GC 60365K->50414K(129664K), 0.0320474 secs]", this));
         parser.parse("", this);
-        assertEquals(4, count);
+        assertEquals(5, count);
     }
 
     public void addValues(double[] values) {
         assertEquals(expected.length, values.length);
-        for(int i=0;i<expected.length;i++){
+        for (int i = 0; i < expected.length; i++) {
             assertEquals(expected[i], values[i]);
         }
         count++;
