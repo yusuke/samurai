79284c4f329ccbd356424c4e7d9456ca33d404af
Yusuke Yamamoto <yusuke@mac.com>
2021-10-13 23:20:25 +0900

    relaunch Samurai if --add-exports / --add-opens options are not specified.

--- samurai-swing/src/main/java/one/cafebabe/samurai/swing/Samurai.java
+++ samurai-swing/src/main/java/one/cafebabe/samurai/swing/Samurai.java
@@ -15,12 +15,18 @@
  */
 package one.cafebabe.samurai.swing;
 
+import one.cafebabe.samurai.remotedump.ProcessUtil;
 import one.cafebabe.samurai.util.GUIResourceBundle;
 import one.cafebabe.samurai.util.OSDetector;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import sun.jvmstat.monitor.MonitorException;
 
 import javax.swing.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Samurai {
@@ -28,6 +34,48 @@ public class Samurai {
     private static final GUIResourceBundle resources = GUIResourceBundle.getInstance();
 
     public static void main(String[] args) {
+        // relaunch Samurai if --add-exports / --add-opens options are not specified.
+        if (args.length != 1 || !args[0].equals("nested-launch")) {
+            try {
+                // check access to sun.jvmstat.* is permitted.
+                ProcessUtil.getVMs("localhost");
+            } catch (IllegalAccessError error) {
+                System.out.println("access class sun.jvmstat.monitor.HostIdentifier (in module jdk.internal.jvmstat)");
+                List<String> command = new ArrayList<>();
+                command.add(System.getProperty("java.home") + "/bin/java");
+                command.add("-classpath");
+                command.add(System.getProperty("java.class.path"));
+                command.add("-Djdk.attach.allowAttachSelf=true");
+                command.add("-Xmx128m");
+                command.add("-Xms128m");
+                command.add("--add-exports=jdk.internal.jvmstat/sun.jvmstat.monitor=ALL-UNNAMED");
+                command.add("--add-exports=jdk.attach/sun.tools.attach=ALL-UNNAMED");
+                command.add("--add-opens=java.desktop/javax.swing=ALL-UNNAMED");
+                command.add("--add-opens=java.desktop/javax.swing.border=ALL-UNNAMED");
+                command.add("--add-opens=java.desktop/com.apple.laf=ALL-UNNAMED");
+                if (OSDetector.isMac()) {
+                    command.add("-Dsun.java2d.metal=true");
+                }
+                if (OSDetector.isWindows()) {
+                    command.add("--add-opens=java.desktop/com.sun.java.swing.plaf.windows=ALL-UNNAMED");
+                }
+                command.add("--add-opens=java.desktop/javax.swing.colorchooser=ALL-UNNAMED");
+                command.add("one.cafebabe.samurai.swing.Samurai");
+                command.add("nested-launch");
+
+                System.out.println("relaunching samurai with the following command:");
+                System.out.println(String.join(" ", command));
+                ProcessBuilder builder = new ProcessBuilder().command(command.toArray(new String[0]));
+                try {
+                    Process process = builder.start();
+                    System.exit(0);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } catch (URISyntaxException | MonitorException e) {
+                e.printStackTrace();
+            }
+        }
         if (OSDetector.isMac()) {
             System.setProperty("apple.awt.application.name", resources.getMessage("Samurai"));
             System.setProperty("apple.laf.useScreenMenuBar", "true");
